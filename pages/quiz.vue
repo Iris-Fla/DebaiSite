9eac72a52be615fb829f662378db5ed06b1559aa
Iris-Fla <irith.fla@gmail.com>
2023-05-30 01:17:39 +0900

    updated

---
+++ pages/quiz.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div>
+    <h1>Quiz</h1>
+    <div class="question">
+      <p>{{ questions[currentQuestion].question }}</p>
+    </div>
+    <div class="answer">
+      <input type="radio" name="answer" value="1" v-model="answer"> {{ questions[currentQuestion].answers[0] }}
+      <br />
+      <input type="radio" name="answer" value="2" v-model="answer"> {{ questions[currentQuestion].answers[1] }}
+      <br />
+      <input type="radio" name="answer" value="3" v-model="answer"> {{ questions[currentQuestion].answers[2] }}
+    </div>
+    <div class="button">
+      <button @click="checkAnswer">Check Answer</button>
+      <button @click="nextQuestion">Next Question</button>
+    </div>
+  </div>
+</template>
+<script setup>
+const questions= ref( [
+        {
+          question: "What is the capital of France?",
+          answers: ["Paris", "London", "New York"],
+          correctAnswer: "Paris",
+        },
+        {
+          question: "What is the population of the Earth?",
+          answers: ["8 billion", "7 billion", "6 billion"],
+          correctAnswer: "8 billion",
+        },
+        {
+          question: "What is the chemical symbol for water?",
+          answers: ["H2O", "H2O2", "H2"],
+          correctAnswer: "H2O",
+        },
+      ])
+const currentQuestion = ref(0)
+const score = ref(0)
+const index = ref(0)
+const checkAnswer = () => {
+      let answer = questions.value[index.value].answers[0];
+      if (answer === questions.value[index.value].correctAnswer) {
+        score + 1;
+      }
+    }
+
+const nextQuestion= () => {
+      let index = currentQuestion + 1;
+      if (index < questions.length) {
+        currentQuestion = index;
+      } else {
+        showResults();
+      }
+    }
+const showResults = () => {
+      var message = "Your score is " + score;
+    }
+</script>
+<style></style>
---
+++ pages/sample.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="container">
+    <div
+      class="card mx-auto my-5 shadow-lg mb-5 bg-body rounded-5"
+      style="max-width: 25rem"
+    >
+      <img
+        :src="questions[NowQuestion].fileName"
+        class="card-img-to rounded-5 rounded-bottom"
+        alt="..."
+      />
+      <div class="card-body mb-3">
+        <p class="card-title text-center cocoa title_text">
+          <span>デババ！</span>
+        </p>
+        <p class="card-text text-center fs-5">
+          {{ currentQuestion }}
+        </p>
+        <div
+          @click="NextQuestion"
+          class="btn hover-effect a mt-4 mx-3 d-flex align-items-center rounded-pill text-center"
+          style="height: 60px"
+          >{{ text }}</div
+        >
+        <div
+          class="btn hover-effect b mt-4 mx-3 d-flex align-items-center rounded-pill text-center"
+          style="height: 60px"
+          >戻る</div
+        >
+        <div
+          class="btn hover-effect c mt-4 mx-3 d-flex align-items-center rounded-pill text-center"
+          style="height: 60px"
+          >戻る</div
+        >
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+
+const questions = ref( [
+    {
+        questionText:"上記の画像の信頼度は？",
+        ans:["70%","77.7%","大当り濃厚"],
+        correctAnswer:"70%",
+        fileName : "/saikyou.jpg"
+    },
+    {
+        questionText:"進んだよ",
+        ans:["70%","77.7%","大当り濃厚"],
+        correctAnswer:"70%",
+        questionImg : "@/public/saikyou.jpg"
+    }
+])
+const text = "BOBBY"
+var NowQuestion = ref(0)
+const NextQuestion = () => {
+    NowQuestion.value ++;
+}
+const currentQuestion = computed(() => {
+  return questions.value[NowQuestion.value].questionText
+})
+</script>
+<style>
+.cocoa {
+  color: #fcbda3;
+}
+.btn {
+  color: var(--bs-white);
+}
+.hover-effect:hover {
+  background-color: #fcbda3;
+}
+.a {
+    background: #87cf91;
+}
+.b {
+    background: #86cfcc;
+}
+.c {
+    background: #c78282;
+}
+.title_text{
+    font-size:80px;
+}
+</style>
